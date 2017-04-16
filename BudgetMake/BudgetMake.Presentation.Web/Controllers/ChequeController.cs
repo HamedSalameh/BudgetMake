b7556c386ec18807062fb100938aa637a741d6ca
hamedsalameh <hamedsalami@gmail.com>
2017-04-16 09:48:47 +0300

    Add Cheque controller

---
+++ BudgetMake/BudgetMake.Presentation.Web/Controllers/ChequeController.cs
@@ -0,0 +1,35 @@
+﻿using BudgetMake.Presentation.Web.Extentions;
+using BudgetMake.Presentation.Web.ViewModel;
+using BudgetMake.Shared.Contracts.Domain;
+using BudgetMake.Shared.Contracts.Infra;
+using BudgetMake.Shared.DomainModel;
+using System.Collections.Generic;
+
+namespace BudgetMake.Presentation.Web.Controllers
+{
+    public class ChequeController : BudgetItemBaseController<Cheque, ChequeViewModel>
+    {
+        public ChequeController(IApplication Application, ILocalLogger Log) : base(Application, Log)
+        {
+            PartialViewNameFor_ItemsList = "Expenses";
+            PartialViewNameFor_CreateItem = "CreateExpenseItem";
+            PartialViewNameFor_EditItem = "EditExpenseItem";
+            PartialViewNameFor_DeleteItem = "DeleteExpenseItem";
+        }
+
+        protected override IList<ChequeViewModel> GetViewModelsList(int MonthlyPlanId)
+        {
+            return application.GetEntities<Cheque>(e => e.MonthlyBudgetId == MonthlyPlanId).MapToViewModelsList();
+        }
+
+        protected override ChequeViewModel MapToViewModel(Cheque model)
+        {
+            return model.MapToViewModel();
+        }
+
+        protected override Cheque MapToModel(ChequeViewModel ViewModel)
+        {
+            return ViewModel.MapToModel();
+        }
+    }
+}
